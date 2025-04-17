d67fbdee2b568e78e12eb4413fe5ad9b4c877764
eWinker <elijah.winker@stu.daviess.kyschools.us>
2025-04-17 12:53:47 +0000

    hytgfd

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,22 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
-
+		Arrays.sort(ray);
+		int bottom=ray[0];
+		int top=ray.length-1;
+		int mid=0;
+		while(bottom<top){
+			mid=(top + bottom)/2;
+			if((ray[mid])<item){
+				bottom=mid+1;
+			}
+			if((ray[mid]>item)){
+				top=mid-1;
+			}
+			if(ray[mid]==item){
+				return mid;
+			}
+		}
+		return -1;
 	}
 }
